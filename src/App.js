4ce57012dad5d4e624ccbe23b4e5d16d1b0c0f56
faenerisis <sahinnkubilay11@gmail.com>
2024-07-14 17:06:03 +0300

    React
    Feature: Frame-work changed Vanilla JS to React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,22 @@
+const Header = () => {
+    return(
+        <div class="sidebar">
+            <div class="head-section">
+                <h1 id="head-title">Kubilay Şahin</h1>
+                <table id="head-table">
+                    <tr>
+                        <th>Jr IT Operations Support Specialist | Software Developer</th>
+                    </tr>
+                    <tr>
+                        <th>-İstanbul Türkiye-</th>
+                    </tr>
+                    <tr>
+                        <td>Contact, mail: sahinnkubilay11@gmail.com</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
